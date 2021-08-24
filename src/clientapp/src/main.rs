b396154dca211669101164e96461c4d2cdb89d27
uglyluigi <5006946+uglyluigi@users.noreply.github.com>
2021-08-24 10:28:48 -0500

    Initial router stuff

--- src/clientapp/src/main.rs
+++ src/clientapp/src/main.rs
@@ -1,5 +1,8 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
+use yew_router::service::RouteService;
+use yew_router::route::RouteState;
+use serde::{Serialize, Deserialize};
 
 
 mod components;
@@ -9,26 +12,46 @@ struct Model {
     // `ComponentLink` is like a reference to a component.
     // It can be used to send messages to the component
     link: ComponentLink<Self>,
+    route_service: RouteService<SaturnRouteState>,
+    route_state: SaturnRouteState,
 }
+
+#[derive(PartialEq, Serialize, Debug, Clone, Default, Deserialize)]
+struct SaturnRouteState { }
 
 #[derive(Debug, Switch, Clone)]
 enum Route {
     #[to = "/"]
+    Index,
+
+    #[to = "/login"]
+    Login,
+
+    #[to = "/home"]
     Home,
-    #[to = "/"]
-    Secure
+
+    #[to = "/404"]
+    NotFound
+}
+
+enum Msg {
+    RouteChanged,
 }
 
 impl Component for Model {
-    type Message = ();
+    type Message = Msg;
+
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link }
+        Self { link, route_service: RouteService::<SaturnRouteState>::new(), route_state: SaturnRouteState::default() }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        false
+        match msg {
+            Msg::RouteChanged => true,
+            _ => false
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -40,24 +63,43 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <Router<Route> render={Router::render(|r: Route| {
-                match r {
-                    Route::Home => html!{ 
-                        <div>
-                            <StellarBg/>
-                            <ToolbarComponent/>
-                            <LoginPageComponent/> 
-                        </div>
-                    },
-        
-                    Route::Secure => html! {
-                        {"Penis!"}
+            <Router<Route, SaturnRouteState> 
+                render = Router::render(|route: Route| {
+                    match route {
+                        Route::Index => {
+                            html! {
+                                // todo: if not logged in...
+                                // for now, redirect to login page
+                                <div>
+                                    <h1>{"Redirecting..."}</h1>
+                                </div>
+                            }
+                        },
+
+                        Route::Home => {
+                            html!{"NYI"}
+                        },
+
+                        Route::Login => html! {
+                            <div>
+                                <StellarBg/>
+                                <ToolbarComponent/>
+                                <LoginPageComponent/>
+                            </div>
+                        },
+
+                        Route::NotFound => html! {
+                            <div>
+                                <h1>{"404 Not Found :("}</h1>
+                            </div>
+                        }
                     }
-                }
-            })} />
+                })
+            />
         }
     }
 }
+
 
 fn main() {
     yew::start_app::<Model>();
