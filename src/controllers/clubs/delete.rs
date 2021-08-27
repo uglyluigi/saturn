e8e595b4d4145f70b29df0c57b1b246be65d72e6
Booglejr <rooty@kwtechllc.com>
2021-08-27 00:47:16 -0500

    Cleanup club members as well.

--- src/controllers/clubs/delete.rs
+++ src/controllers/clubs/delete.rs
@@ -3,8 +3,12 @@ use crate::prelude::*;
 #[get("/clubs/<id>", rank=1)]
 pub async fn delete_admin(_admin: Admin, db: Db, id: i32) -> Result<()> {
     use crate::schema::clubs::dsl::{clubs};
+    use crate::schema::club_members::dsl::{club_members, club_id};
 
     db.run(move |conn| {
+        diesel::delete(club_members.filter(club_id.eq(id)))
+                .execute(conn)
+                .expect("Couldn't delete clubs_members prior to club deletion from database.");
         diesel::delete(clubs.find(id))
             .execute(conn)
             .expect("Couldn't delete clubs from database.");
@@ -25,7 +29,12 @@ pub async fn delete_user(user: User, db: Db, id: i32) -> Result<()> {
             .filter(is_moderator.eq(true))
             .limit(1).load::<ClubMember>(conn);
 
+        
+
         if relation.is_ok() {
+            diesel::delete(club_members.filter(club_id.eq(id)))
+                .execute(conn)
+                .expect("Couldn't delete clubs_members prior to club deletion from database.");
             diesel::delete(clubs.find(id))
                 .execute(conn)
                 .expect("Couldn't delete clubs from database.");
