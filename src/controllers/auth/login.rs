4998dff3b5151e7c6afdee5b307225a716b897b6
Booglejr <rooty@kwtechllc.com>
2021-08-26 19:10:45 -0500

    Debug csrf token

--- src/controllers/auth/login.rs
+++ src/controllers/auth/login.rs
@@ -10,6 +10,8 @@ pub struct GoogleTokenForm<'r> {
 pub async fn login(token: Form<GoogleTokenForm<'_>>, cookies: &CookieJar<'_>) -> Redirect {
     let cred = token.credential.clone();
     let cookies_g_csrf_token = cookies.get("g_crsf_token").map(|c| c.value().to_string()).unwrap_or(String::new());
+    println!("Cookies: {}", cookies_g_csrf_token);
+    println!("Token: {}", token.g_csrf_token);
     if cookies_g_csrf_token == token.g_csrf_token{
         let cookie = Cookie::new("user_jwt", cred.to_owned()); 
         cookies.add_private(cookie);
