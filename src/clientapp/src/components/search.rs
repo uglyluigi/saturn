b671983bb69f09fcb7c38b0e50c05f3f1deb78b2
Booglejr <rooty@kwtechllc.com>
2021-09-30 16:04:30 -0500

    Make stuff look dope and work

---
+++ src/clientapp/src/components/search.rs
@@ -0,0 +1,56 @@
+use yew::{prelude::*, Properties};
+use crate::types::*;
+use crate::{
+	components::core::{router::*, Toolbar}
+};
+
+pub struct SearchBar {
+	link: ComponentLink<Self>,
+    props: Props,
+}
+
+pub enum Msg {
+	DoZeTypeyType(String),
+}
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct Props {
+
+}
+
+impl Component for SearchBar {
+    type Message = Msg;
+	type Properties = Props;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            props
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+			Msg::DoZeTypeyType(search) => {
+
+			},
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+		false
+	}
+
+    fn view(&self) -> Html {
+        html!{
+            <>
+            <Toolbar username="Baby Seal Clubber"/>
+            <div class="search-bar-container">
+                <h1 class="search-bar-h1"> {"find something "} <i>{" totally "}</i> {" you."} </h1>
+                <input class="search-bar-input" placeholder="I'm looking for..."/>
+            </div>
+            </>
+        }
+    }
+}
