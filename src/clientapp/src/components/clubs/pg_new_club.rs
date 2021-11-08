230e5919474c0e3fa44b2a4edfe3ba9ad9ca26b9
uglyluigi <5006946+uglyluigi@users.noreply.github.com>
2021-11-07 23:03:43 -0600

    Club Page 2.0

--- src/clientapp/src/components/clubs/pg_new_club.rs
+++ src/clientapp/src/components/clubs/pg_new_club.rs
@@ -1,9 +1,11 @@
+use std::fmt::Display;
+
 use anyhow::anyhow;
-use comrak::{markdown_to_html, ComrakExtensionOptions, ComrakOptions};
+use comrak::{ComrakExtensionOptions, ComrakOptions, arena_tree::Node, markdown_to_html};
 
 use serde_json::{json};
 use wasm_bindgen::{JsCast, prelude::Closure};
-use web_sys::{Blob, FileReader, HtmlElement, HtmlImageElement, HtmlInputElement};
+use web_sys::{Blob, FileReader, HtmlElement, HtmlImageElement, HtmlInputElement, HtmlTextAreaElement};
 use yew::{
 	format::{Bincode, Json, Nothing},
 	prelude::*,
@@ -37,6 +39,9 @@ pub struct NewClubPage {
 	img_preview_ref: NodeRef,
 	markdown_preview_ref: NodeRef,
 	club_name_input_ref: NodeRef,
+
+	form_errors: Option<Vec<FormError>>,
+	markdown_textarea_ref: NodeRef,
 }
 
 #[derive(Properties, Debug, Clone)]
@@ -52,6 +57,7 @@ pub enum Msg {
 
 	PostClub,
 	PostClubDone(i64),
+	Reset
 }
 
 pub enum WhichTextField {
@@ -66,20 +72,58 @@ pub enum ReadImgResult {
 	EmptyFileList,
 }
 
-impl NewClubPage {
-	fn get_sanitized_md(&mut self) {}
+#[derive(Clone)]
+pub enum FormError {
+	ClubName(String),
+	ClubDescription(String),
+	ClubLogo(String),
+}
 
+impl FormError {
+	fn unwrap(self) -> String {
+		match self {
+			Self::ClubName(string) => {
+				string
+			},
+
+			Self::ClubDescription(string) => {
+				string
+			},
+
+			Self::ClubLogo(string) => {
+				string
+			}
+		}
+	}
+}
+
+impl NewClubPage {
 	fn reset(&mut self) {
 		self.club_body_field_contents = None;
 		self.club_name_field_contents = None;
 		self.long_club_description_contents = None;
 		self.post_task_state = FetchState::Waiting;
+		self.post_task = None;
+		if self.form_errors.is_some() {
+			self.form_errors.take(); //Take and drop
+		}
 
-		//self.img_preview_ref.cast::<HtmlImageElement>().unwrap().set_src("");
+		self.img_preview_ref.cast::<HtmlImageElement>().unwrap().set_src("");
+		self.club_name_input_ref.cast::<HtmlInputElement>().unwrap().set_value("");
+		self.markdown_textarea_ref.cast::<HtmlTextAreaElement>().unwrap().set_value("");
+
 		self.markdown_preview_ref
 			.cast::<HtmlElement>()
 			.unwrap()
 			.set_inner_html("");
+	}
+
+	fn add_form_error(&mut self, e: FormError) {
+		if let Some(v) = self.form_errors.as_mut() {
+			v.push(e);
+		} else {
+			self.form_errors = Some(vec![e]);
+		}
 	}
 
 	fn read_img_sync(&self) {
@@ -113,6 +157,54 @@ impl NewClubPage {
 			}
 		}
 	}
+
+	fn get_form_errors(&self, e: FormError) -> Html {
+		use FormError::*;
+		let mut ret_errors: Vec<FormError> = vec![];
+
+		if let Some(errors) = &self.form_errors {
+			for error in errors {
+				if let ClubLogo(_) = error {
+					if let ClubLogo(_) = e {
+						ret_errors.push(error.clone());
+					}
+				} 
+
+				if let ClubName(_) = error {
+					if let ClubName(_) = e {
+						ret_errors.push(error.clone());
+					}
+				}
+
+				if let ClubDescription(_) = error {
+					if let ClubDescription(_) = e {
+						ret_errors.push(error.clone());
+					}
+				}
+			}
+
+			html! {
+				<>
+					{
+						for ret_errors.iter().map(|x| {
+							html! {
+								<h5>
+									{
+										x.clone().unwrap()
+									}
+								</h5>
+							}
+						})
+					}
+				</>
+			}
+		} else {
+			html! {
+				<>
+				</>
+			}
+		}
+	}
 }
 
 impl Component for NewClubPage {
@@ -135,6 +227,8 @@ impl Component for NewClubPage {
 			club_name_input_ref: NodeRef::default(),
 			post_logo_task: None,
 			post_logo_task_state: FetchState::Waiting,
+			form_errors: None,
+			markdown_textarea_ref: NodeRef::default(),
 		}
 	}
 
@@ -178,8 +272,32 @@ impl Component for NewClubPage {
 			},
 
 			Msg::ValidateForm => {
-				// TODO
-				self.link.send_message(Msg::PostClub);
+				let mut v = vec![];
+
+				if self.club_name_field_contents.is_none() {
+					v.push(FormError::ClubName("Club name cannot be empty.".to_owned()));
+				}
+
+				if self.long_club_description_contents.is_none() {
+					v.push(FormError::ClubDescription("Club description cannot be empty.".to_owned()));
+				}
+
+				if let Some(el) = self.img_preview_ref.cast::<HtmlImageElement>() {
+
+					let src = el.src();
+
+					if src.is_empty() {
+						v.push(FormError::ClubLogo("Club logo cannot be empty.".to_owned()));
+					} else if src.bytes().len() > 1000000 {
+						v.push(FormError::ClubLogo(format!("Club logo is too large. ({} MB > 1MB)", src.bytes().len() as f32 / 1000000.0)));
+					}
+				}
+
+				if v.is_empty() {
+					self.link.send_message(Msg::PostClub);
+				} else {
+					self.form_errors = Some(v);
+				}
 			}
 
 			Msg::PostClub => {
@@ -192,18 +310,19 @@ impl Component for NewClubPage {
 					//FIXME back end often returns 422 on markdown with newlines and probably other stuff
 					// Clean your body with ammonia
 					let json = json!({"name": name, "body": ammonia::clean(&body)});
-					let request = Request::post("/api/clubs/create")
+
+					let request = Request::post("http://localhost:443/api/clubs/create")
 						.body(Json(&json))
 						.unwrap();
 
 					let response_callback = self.link.callback(
-						|response: Response<Json<Result<(), anyhow::Error>>>| {
+						|response: Response<Json<Result<String, anyhow::Error>>>| {
 							match response.status() {
 								StatusCode::OK => {
 									tell!("Successfully post`ed club");
 									tell!("{:?}", response);
 									Msg::PostClubDone(20202)
-								}
+								},
 
 								_ => {
 									tell!("Bad status receieved: {:?}", response.status());
@@ -226,7 +345,6 @@ impl Component for NewClubPage {
 			}
 
 			Msg::PostClubDone(id) => {
-				crate::tell!("Mingus");
 				self.reset();
 				//self.link.send_message(Msg::PostClubLogo(id));
 			}
@@ -236,7 +354,9 @@ impl Component for NewClubPage {
 
 				match std::str::from_utf8(&bytes[0..]) {
 					Ok(src) => el.set_src(src),
-					Err(_) => crate::tell!("Something bad happen!"),
+					Err(_) => {
+						self.add_form_error(FormError::ClubLogo("Invalid image.".to_owned()))
+					},
 				}
 			}
 
@@ -244,6 +364,12 @@ impl Component for NewClubPage {
 
 			Msg::ReadLogo => {
 				self.read_img_sync();
+			},
+
+			Msg::Reset => {
+				if gloo_dialogs::confirm("Are you sure you want to clear all fields? This cannot be undone.") {
+					self.reset();
+				}
 			}
 		}
 		true
@@ -263,7 +389,13 @@ impl Component for NewClubPage {
 			Msg::UpdateInfoState(WhichTextField::TheLongDescriptionOne, data.value)
 		});
 
-		let submit_cb = self.link.callback(|cock| Msg::PostClub);
+		let submit_cb = self.link.callback(|_| {
+			Msg::ValidateForm
+		});
+
+		let reset_cb = self.link.callback(|_| {
+			Msg::Reset
+		});
 
 		let image_input_callback = self
 			.link
@@ -275,13 +407,28 @@ impl Component for NewClubPage {
 					<h1>{"Create a new club!"}</h1>
 					<div class="image-input">
 						<img ref=self.img_preview_ref.clone() class="club-logo"/>
-						<input ref=self.img_selector_ref.clone() oninput=image_input_callback type="file" name="file" id="file" class="inputfile"/>
+						<input ref=self.img_selector_ref.clone() oninput=image_input_callback type="file" name="file" id="file" class="inputfile" accept="image/png"/>
 						<label for="file">{"Select a club logo"}</label>
-						<small>{"(png files only. <= 5MB in size)"}</small>
+						<small>{"(png files only. <= 1MB in size)"}</small>
 					</div>
-					<input oninput=club_name_field_cb class="club-input" type="text" placeholder="Club name"/>
+					<div class="form-errors">
+						{
+							self.get_form_errors(FormError::ClubLogo("".to_owned()))
+						}
+					</div>
+					<input ref=self.club_name_input_ref.clone() oninput=club_name_field_cb class="club-input" type="text" placeholder="Club name"/>
+					<div class="form-errors">
+						{
+							self.get_form_errors(FormError::ClubName("".to_owned()))
+						}
+					</div>
 					<h3>{"Club description (markdown supported)"}</h3>
-					<textarea oninput=description_cb class="markdown-textarea"/>
+					<textarea ref=self.markdown_textarea_ref.clone() oninput=description_cb class="markdown-textarea"/>
+					<div class="form-errors">
+						{
+							self.get_form_errors(FormError::ClubDescription("".to_owned()))
+						}
+					</div>
 					<span>
 						<button class="normal-button submit-new-club-button" onclick=submit_cb>{"Submit"}</button>
 						{
@@ -305,7 +452,9 @@ impl Component for NewClubPage {
 								}
 							} else {
 								html! {
-									<></>
+									<>
+										<button class="normal-button destructive-button reset-club-page-button" onclick=reset_cb>{"Reset"}</button>
+									</>
 								}
 							}
 						}
@@ -324,7 +473,7 @@ impl Component for NewClubPage {
 
 	fn rendered(&mut self, first: bool) {
 		if first {
-			//self.club_name_input_ref.cast::<HtmlElement>().unwrap().focus().unwrap();
+			self.club_name_input_ref.cast::<HtmlElement>().unwrap().focus().unwrap();
 		}
 	}
 }
