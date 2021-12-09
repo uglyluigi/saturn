7759dea21926e7538c745d6fd75575cb6ccb2285
Ashlynn <ashlynn.martell@selu.edu>
2021-12-08 22:11:15 -0600

    Format imports, cleanup

--- src/clientapp/src/event/amogus.rs
+++ src/clientapp/src/event/amogus.rs
@@ -1,61 +1,62 @@
-use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
+
+use serde::{Deserialize, Serialize};
 use yew::worker::*;
+
 use crate::tell;
 
 pub type Amogus = EventBus;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Request {
-    EventBusMsg(AgentMessage)
+	EventBusMsg(AgentMessage),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum AgentMessage {
-    ToolbarMsg(crate::components::core::toolbar::Msg),
-    DetailsPageMsg(crate::components::clubs::pg_details::Msg),
-    ClubFormMsg(crate::components::pg_new_club::Msg)
+	ToolbarMsg(crate::components::core::toolbar::Msg),
+	DetailsPageMsg(crate::components::clubs::pg_details::Msg),
+	ClubFormMsg(crate::components::pg_new_club::Msg),
 }
 
 pub struct EventBus {
-    link: AgentLink<EventBus>,
-    subscribers: HashSet<HandlerId>,
+	link: AgentLink<EventBus>,
+	subscribers: HashSet<HandlerId>,
 }
 
 impl Agent for EventBus {
-    type Reach = Context<Self>;
-    type Message = ();
-    type Input = Request;
-    type Output = AgentMessage;
+	type Reach = Context<Self>;
+	type Message = ();
+	type Input = Request;
+	type Output = AgentMessage;
 
-    fn create(link: AgentLink<Self>) -> Self {
-        Self {
-            link,
-            subscribers: HashSet::new(),
-        }
-    }
+	fn create(link: AgentLink<Self>) -> Self {
+		Self {
+			link,
+			subscribers: HashSet::new(),
+		}
+	}
 
-    fn update(&mut self, _msg: Self::Message) {}
+	fn update(&mut self, _msg: Self::Message) {}
 
-    fn handle_input(&mut self, msg: Self::Input, _id: HandlerId) {
+	fn handle_input(&mut self, msg: Self::Input, _id: HandlerId) {
+		match msg {
+			Request::EventBusMsg(s) => {
+				for sub in self.subscribers.iter() {
+					tell!("{:?} sent {:?}", sub, s);
+					self.link.respond(*sub, s.clone());
+				}
+			}
+		}
+	}
 
-        match msg {
-            Request::EventBusMsg(s) => {
-                for sub in self.subscribers.iter() {
-                    tell!("{:?} sent {:?}", sub, s);
-                    self.link.respond(*sub, s.clone());
-                }
-            }
-        }
-    }
+	fn connected(&mut self, id: HandlerId) {
+		tell!("connecting id={:?}", id);
+		self.subscribers.insert(id);
+		tell!("inserted");
+	}
 
-    fn connected(&mut self, id: HandlerId) {
-        tell!("connecting id={:?}", id);
-        self.subscribers.insert(id);
-        tell!("inserted");
-    }
-
-    fn disconnected(&mut self, id: HandlerId) {
-        self.subscribers.remove(&id);
-    }
+	fn disconnected(&mut self, id: HandlerId) {
+		self.subscribers.remove(&id);
+	}
 }
