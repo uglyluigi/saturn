3b6a696d1913f84eab9f3628d7d00ddf0d8b0c34
Ashlynn <ashlynn.martell@selu.edu>
2021-12-08 22:07:43 -0600

    Format imports, cleanup

--- src/clientapp/src/components/clubs/mod.rs
+++ src/clientapp/src/components/clubs/mod.rs
@@ -1,11 +1,11 @@
 pub mod club_card;
 pub mod club_view;
-pub mod pg_new_club;
 pub mod pg_details;
+pub mod pg_new_club;
 pub mod search;
 
 pub use club_card::ClubCard;
 pub use club_view::ClubView;
-pub use pg_new_club::NewClubPage;
 pub use pg_details::DetailsPage;
+pub use pg_new_club::NewClubPage;
 pub use search::SearchBar;
